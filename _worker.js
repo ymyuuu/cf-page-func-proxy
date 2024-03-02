a5fc461d4465409a168881ee994aa57b6e8b51ad
Mingyu <135582157+ymyuuu@users.noreply.github.com>
2024-03-02 17:00:01 +0800

    Update _worker.js

--- _worker.js
+++ _worker.js
@@ -1,7 +1,7 @@
 export default {
     async fetch(request, env) {
       let url = new URL(request.url);
-      if (url.pathname.startsWith('/')) {
+      if (url.pathname.startsWith('onecn.eu.org')) {
         url.hostname="example.com";
         let new_request=new Request(url,request);
         return fetch(new_request);
